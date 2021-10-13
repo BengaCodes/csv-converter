55b945bd19b8fa5b293133811711862b975089c4
Benga <benga.olasebikan@gmail.com>
2021-10-13 21:13:44 +0100

    pulled out required columns and rows and stored in a new array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Fragment, useState } from 'react'
+import extractNumbers from 'extract-numbers'
 import CsvReader from './components/CsvReader'
+
+
+
+let filteredColumns = []
 
 function App() {
   const [data, setData] = useState([])
@@ -10,6 +15,22 @@ function App() {
 
 
   console.log('Data: ', data)
+
+
+  // ! Pulling out the columns and rows I require for my download
+  data[0]?.forEach((val, i) => {
+    if (val === 'ISIN' || val === 'CFICode' || val === 'Venue' || val === 'AlgoParams') {
+      data.slice(1).forEach(arr => {
+        const obj = {}
+        obj[val] = arr[i]
+        filteredColumns.push(obj)
+      })
+    }
+  })
+
+  filteredColumns =  filteredColumns.map(val => val.column === 'AlgoParams' ? { ...val, column: 'ContractSize', rows: +extractNumbers(val.rows.split(';').filter(pm => pm.includes('PriceMultiplier'))[0])[0] } : val)
+
+  console.log(filteredColumns)
 
 
   return (
